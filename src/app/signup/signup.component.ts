45d32ee9147b59da5353f782733e9996404f44d8
DongKeun1 <skj2366@naver.com>
2020-02-07 03:47:11 +0900

    email certification, but suddenly error

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -11,6 +11,10 @@ import { CustomerInfo } from '../vo/customer-info';
 export class SignupComponent implements OnInit {
 
   cui: CustomerInfo = new CustomerInfo();
+  isSend: boolean = false;
+  isCerti: boolean = false;
+  certificationNumber: string = '';
+  certifi: string = '';
 
   constructor(private _router:Router, private _cs:CommonService) { }
 
@@ -30,5 +34,29 @@ export class SignupComponent implements OnInit {
         alert('singup error');
       }
     )
+  }
+
+  doSendMail() {
+    this._cs.postJson('/send', this.cui).subscribe(
+      res => {
+        console.log(res);
+        console.log(this.cui.cuiEmail);
+        alert('메일 전송!');
+        this.isSend = true;
+        this.certificationNumber = <string>res;
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  confirmCertificationNumber() {
+    
+    if(this.certificationNumber == this.certifi) {
+      this.isCerti = true;
+    }else {
+      alert('인증번호를 확인해주세요');
+    }
   }
 }
