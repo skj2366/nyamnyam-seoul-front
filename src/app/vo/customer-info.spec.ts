ba2fd93b4aac312fc3ca22d964d82f2404c65c69
ES09 <e.song925@gmail.com>
2020-02-07 15:10:20 +0900

    main 약간 수정

--- src/app/vo/customer-info.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CustomerInfo } from './customer-info';
-
-describe('CustomerInfo', () => {
-  it('should create an instance', () => {
-    expect(new CustomerInfo()).toBeTruthy();
-  });
-});
