f3112f626c07bb163085e66875d731c01855f537
DongKeun, Kim <tutgame2089@gmail.com>
2020-02-09 17:36:47 +0900

    for pull

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -36,27 +36,17 @@ export class SignupComponent implements OnInit {
     )
   }
 
-  // doSendMail() {
-  //   this._cs.postJson('/send', this.cui).subscribe(
-  //     res => {
-  //       console.log(res);
-  //       console.log(this.cui.cuiEmail);
-  //       alert('메일 전송!');
-  //       this.isSend = true;
-  //       this.certificationNumber = <string>res;
-  //     },
-  //     err => {
-  //       console.log(err);
-  //     }
-  //   )
-  // }
-
-  checkEmail() {
-    this._cs.get('').subscribe(
+  checkEmail(): any {
+    this._cs.get('/cui/cuc/email', this.cui.cuiEmail).subscribe(
       res => {
-        if(!res) {
+        if (!res) {
           console.log(res);
-          
+
+          return 1;
+        } else {
+          console.log(res);
+          alert('이미 가입된 이메일 입니다');
+          return 2;
         }
       }
     )
@@ -67,7 +57,13 @@ export class SignupComponent implements OnInit {
     // 여기에 인증번호 보낸 이력 있으면 isSend 바로 True 시키고 이메일 다시 안보내는 로직 추가 
     // 그리고 인증번호 재전송 로직 추가
 
-    
+    if (this.checkEmail() == 1) {
+      console.log("this is do check");
+      return "qwe";
+    } else {
+      console.log("this is else check");
+      return "zxc";
+    }
 
     this._cs.postResString('/send', this.cui).subscribe(
       res => {
@@ -88,16 +84,16 @@ export class SignupComponent implements OnInit {
     this._cs.get('/cuc/' + this.cui.cuiEmail).subscribe(
       res => {
         console.log(res);
-        if(!res) {
+        if (!res) {
           alert('인증번호가 없습니다');
-        }else {
-          if(this.certificationNumber == res['cucCerNum']){
+        } else {
+          if (this.certificationNumber == res['cucCerNum']) {
             alert('인증번호 확인 완료!');
             this.isCerti = true;
-          }else {
+          } else {
             alert('인증번호를 다시 확인해 주세요');
           }
-          
+
         }
       },
       err => {
