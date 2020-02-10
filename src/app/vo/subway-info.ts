86d0df71ae45e759b979657208e9f3a1e93eba9c
kumjoo <kumjoo_92@naver.com>
2020-02-10 18:13:52 +0900

    서브우ㅐ이

---
+++ src/app/vo/subway-info.ts
@@ -0,0 +1,22 @@
+export class SubwayInfo {
+    subwayNum : number;
+    subwayName : String;
+    subwayLine1 : number;
+    subwayLine2 : number;
+    subwayLine3 : number;
+    subwayLine4 : number;
+    subwayLine5 : number;
+    subwayLine6 : number;
+    subwayLine7 : number;
+    subwayLine8 : number;
+    subwayLine9 : number;
+    subwayLine10 : number;
+    subwayLine11 : number;
+    subwayLine12 : number;
+    subwayLine13 : number;
+    subwayLine14 : number;
+    subwayLine15 : number;
+    zoneNum : number;
+
+
+}
