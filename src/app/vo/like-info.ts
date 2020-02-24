43a86164ad1f5423c602fb9f11305236f53b0ea1
ES09 <e.song925@gmail.com>
2020-02-24 18:11:46 +0900

    탭 추가 / vo 추가

---
+++ src/app/vo/like-info.ts
@@ -0,0 +1,7 @@
+export class LikeInfo {
+    liiNum : number;
+    cuiNum : number;
+    relNum : number;
+    liiCredat : string;
+    liiCretim : string;
+}
---
+++ src/app/vo/theme-list.ts
@@ -0,0 +1,11 @@
+export class ThemeList {
+    thlNum : number;
+    relNum : number;
+    thlSolo : number;
+    thlCouple : number;
+    thlFour : number;
+    thlCredat : string;
+    thlCretim : string;
+    thlModdat : string;
+    thlModtim : string;
+}
