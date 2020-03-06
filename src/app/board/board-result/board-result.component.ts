dfb5fdbc1dbf53af6f65e01c64e2cafcb9223d1f
DongKeun, Kim <tutgame2089@gmail.com>
2020-03-06 10:49:18 +0900

    [innerHTML] CSS 안먹혀서 수정

--- src/app/board/board-result/board-result.component.ts
+++ src/app/board/board-result/board-result.component.ts
@@ -10,50 +10,25 @@ import { ReviewInfo } from 'src/app/vo/review-info';
   styleUrls: ['./board-result.component.css']
 })
 export class BoardResultComponent implements OnInit {
-  reviewTitle : string = '!';
-  reviewDate : string = '!';
-  reviewRestaurant : string = '!';
-  userNick : string = '!';
-  visitDate : string = '!';
-  visitTime : string = '!';
-  reviewContents : string = '!';
-
-  reviewNum : string = '';
-  rei : ReviewInfo;
-
+  rei : ReviewInfo = new ReviewInfo();
   reiNum : number;
   constructor(private _cs : CommonService, private route : ActivatedRoute) {
     this.reiNum = this.route.snapshot.params['boardNum'];
   }
 
-  ngOnInit() {
-    //var reiNum = this.route.snapshot.paramMap.get('reiNum');
-    // var reiNum = '0';
-    // this.reviewNum = reiNum;
+  async ngOnInit() {
     this.getReviewDetail(this.reiNum);
   }
-
-  // getReviewDetail(reviewNum){
-  //   var url = `/rei/${reviewNum}`;
-  //   this._cs.get(url).subscribe(
-  //     res => {
-  //       console.log(res);
-  //       this.rei = <ReviewInfo>res;
-  //       console.log(this.rei);
-  //       // this.reviewTitle = this.rei.reiTitle;
-  //       // this.reviewDate = this.rei.reiDate + this.rei.reiTime;        
-  //     },
-  //     err => {
-  //       console.log('리뷰상세 못가져옴');
-  //       console.log(err);
-  //     }
-  //   )
-  // }
 
   async getReviewDetail(reviewNum) {
     var url = `/rei/${reviewNum}`;
     this.rei = <ReviewInfo> await this._cs.get(url).toPromise();
     console.log(this.rei);
+    if(this.rei) {
+      document.getElementById('boardContent').innerHTML = this.rei['reiContents'];
+    }
   }
+
+  test: string = `<p style="color:red;">asdasdasd</p>`
    
 }
