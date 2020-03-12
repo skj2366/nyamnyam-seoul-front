2efefb630a0451182e8e0ae4116878746c5a6718
ES09 <e.song925@gmail.com>
2020-03-12 17:55:20 +0900

    blog 추가

---
+++ src/app/vo/restaurant-blog.ts
@@ -0,0 +1,10 @@
+export class RestaurantBlog {
+    
+    blogNum : number;
+    relNum : number;
+    blogContentTitle : string;
+    blogContentUrl : string;
+    blogContentCredat : string;
+    blogCredat : string;
+    blogCretim : string;
+}
