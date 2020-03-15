ef36a302022c956dde37774eaf067e5546965524
DongKeun, Kim <tutgame2089@gmail.com>
2020-03-15 22:39:47 +0900

    좋아요

--- src/app/search/result/result.component.ts
+++ src/app/search/result/result.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { MenuInfo } from 'src/app/vo/menu-info';
 import { KakaoMapService } from 'src/app/common/kakao-map.service';
 import { RestaurantBlog } from 'src/app/vo/restaurant-blog';
+import { StorageService } from 'src/app/common/storage.service';
+import { LikeInfo } from 'src/app/vo/like-info';
 
 @Component({
   selector: 'app-result',
@@ -16,17 +18,19 @@ export class ResultComponent implements OnInit {
   blog : RestaurantBlog[];
   menu : MenuInfo[];
   rNum : number;
+  like : boolean = false;
+  lii : LikeInfo;
   
   menuLength: number;
 
-  constructor(private route : ActivatedRoute, private _cs : CommonService, private _ks: KakaoMapService) { }
+  constructor(private route : ActivatedRoute, private _cs : CommonService, private _ks: KakaoMapService, private _ss: StorageService) { }
 
   async ngOnInit() {    
     var relNum = this.route.snapshot.paramMap.get('relNum');    
     await this.getRestaurantDetail(relNum);
     this.getRestaurantBlogDetail(relNum);
     this.getMenu(relNum);
-    
+    this.getLike();
   }
 
   async getRestaurantDetail(rNum) {
@@ -79,6 +83,54 @@ export class ResultComponent implements OnInit {
       alert(`${this.rel.relLatitude}, ${this.rel.relLongitude}`);
       this._ks.makeMapByRestaurant(this.rel.relLatitude, this.rel.relLongitude, id);
     }
+  }
+
+  likeRestaurant() {
+    // alert('좋아요~');
+    // this.like = true;
+
+    const url = `/lii`;
+    var cuiNum = this._ss.getSession('cuiNum');
+    let writeLike = new LikeInfo();
+    writeLike.cuiNum = Number(cuiNum);
+    writeLike.relNum = this.rel.relNum;
+    this._cs.postJson(url, writeLike).subscribe(
+      res=>{
+        console.log(res);
+        this.getLike();
+      }
+    )
+  }
+
+  unLikeRestaurant() {
+    // alert('좋아요~ 취소');
+    // this.like = false;
+
+    const url = `/lii/${this.lii.liiNum}`;
+    this._cs.delete(url).subscribe(
+      res => {
+        console.log(res);
+        if(res) {
+          this.like = false;
+        }
+      }
+    )
+  }
+
+  getLike() {
+    const url = `/lii/${this._ss.getSession('cuiNum')}/${this.rel.relNum}`;
+    this._cs.get(url).subscribe(
+      res=> {
+        console.log(res);
+        if(res) {
+          this.like = true;
+          this.lii = <LikeInfo>res;
+
+        }else {
+          this.like = false;
+        }
+      }
+    )
   }
 
 }
